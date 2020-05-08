a37da09789ebbf4e22a0f6096b616966a9c3bb82
Victor Frederik Nielsen <vfn2002@gmail.com>
2020-05-08 22:16:52 +0200

    Initial commit

---
+++ _nuxt/15eecbc810353afd2b33.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{294:function(n,e,t){"use strict";t.r(e);var l={name:"Index"},c=t(31),component=Object(c.a)(l,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);e.default=component.exports}}]);
